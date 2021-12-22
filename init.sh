df9cc9a9fae5e8d22c81bb98c0c32b7d9c375e7e
antoine@thefurnitureguys.com <antoine@thefurnitureguys.ca>
2021-12-22 17:26:30 -0500

    update init sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-pip install -q git+https://github.com/huggingface/transformers.git
-pip install google-cloud-storage
+pip3 install -q git+https://github.com/huggingface/transformers.git
+pip3 install google-cloud-storage
 export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+python3 main.py
 
